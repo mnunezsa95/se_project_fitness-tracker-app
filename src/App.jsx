fded73bd22a0a2865a47b5571131297fe7b188ce
mnunezsa <mnunezsa95@gmail.com>
2024-04-19 15:54:07 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import ToggleSwitch from "./components/ToggleSwitch";
-import { ColorModeProvider } from "./contexts/ColorModeProvider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Paper, Switch } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [mode, setMode] = useState(prefersDarkMode);
+  const appTheme = createTheme({
+    palette: {
+      mode: mode ? "dark" : "light",
+    },
+  });
+
+  const handleToggleChange = () => {
+    if (mode) setMode(false);
+    else setMode(true);
+  };
+
   return (
     <>
-      <ColorModeProvider>
-        <div className="dark">
-          <ToggleSwitch />
-        </div>
-      </ColorModeProvider>
+      <ThemeProvider theme={appTheme}>
+        <Paper elevation={0} sx={{ height: "100vh" }} square>
+          <Switch checked={mode} onChange={handleToggleChange} inputProps={{ "aria-label": "controlled" }} />
+          <h1 className="text-[50px]">Dark Mode</h1>
+        </Paper>
+      </ThemeProvider>
     </>
   );
 }
