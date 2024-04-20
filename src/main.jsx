d0b2cc291e516f83802b535f3a9e7a1a1472d711
mnunezsa <mnunezsa95@gmail.com>
2024-04-20 10:34:15 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../src/global/default.css";
-import "../src/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
