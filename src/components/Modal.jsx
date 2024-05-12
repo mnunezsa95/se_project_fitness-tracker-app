4fd72eda9385f06c0e21bab1790ed2adcbafc683
mnunezsa <mnunezsa95@gmail.com>
2024-05-12 15:05:06 -0400

    add form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,28 +1,31 @@
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton } from "@mui/material";
-
+import { Dialog, DialogTitle, IconButton } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import CloseIcon from "@mui/icons-material/Close";
 import { validateModalComponent } from "../validation/propTypes";
+import { RegisterForm } from "./RegisterForm";
 
 const Modal = ({ openModal, handleCloseModal }) => {
   Modal.propTypes = validateModalComponent;
   const theme = useTheme();
 
   return (
-    <div>
-      <Dialog open={openModal} onClose={handleCloseModal} fullWidth>
-        <DialogTitle style={{ fontFamily: theme.palette.typography }}>
-          Sign up
-          <IconButton style={{ float: "right" }}>
-            <CloseIcon color="primary"></CloseIcon>
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText></DialogContentText>
-        </DialogContent>
-        <DialogActions></DialogActions>
-      </Dialog>
-    </div>
+    <Dialog open={openModal} onClose={handleCloseModal} fullWidth>
+      <DialogTitle
+        style={{
+          fontFamily: theme.typography.fontFamily,
+          color: theme.palette.primary.main, // Use primary color for text
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        Sign up for a free account
+        <IconButton onClick={handleCloseModal} color="primary">
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <RegisterForm />
+    </Dialog>
   );
 };
 
