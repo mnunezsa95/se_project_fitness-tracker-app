b4a4a5ec454a11062ede092a41b15e5811dc21bf
mnunezsa <mnunezsa95@gmail.com>
2024-07-16 08:56:51 -0400

    add API call

--- src/components/Workouts/ExerciseSearchbar.jsx
+++ src/components/Workouts/ExerciseSearchbar.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const ExerciseSearchbar = () => {
-  const handleSubmit = () => {};
-  const searchTerm = () => {};
-  const handleChange = () => {};
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => setSearch(e.target.value.toLowerCase());
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(search);
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      console.log(exercisesData);
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="flex items-center space-x-2">
+    <form className="flex items-center space-x-2">
       <input
         type="text"
+        value={search}
+        onChange={handleSearch}
         placeholder="Search Exercises..."
         className="px-3 py-1 border rounded-lg dark:bg-backgroundAccent  placeholder:text-md placeholder:text-content text-content outline-none border-content border-opacity-60 font-Mona-Sans"
       />
       <button
         type="submit"
         className="px-3 py-1 text-white bg-content rounded-lg shadow-sm hover:opacity-75 focus:outline-none focus:ring-2 focus:ring-content font-Mona-Sans"
+        onClick={handleSubmit}
       >
         Search
       </button>
---
+++ src/components/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": import.meta.env.VITE_EXERCISE_DB_API_KEY,
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
