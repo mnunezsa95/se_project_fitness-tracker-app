9728967d4975c4a581f44001698e4251ad31baed
mnunezsa <mnunezsa95@gmail.com>
2024-07-16 13:22:35 -0400

    add carousel

--- src/components/Workouts/ExerciseBodyPartCard.jsx
+++ src/components/Workouts/ExerciseBodyPartCard.jsx
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 const ExerciseBodyPartCard = ({ bodyPartName, bodyPartImage }) => {
   return (
     <div className="flex flex-col items-center gap-y-5">
-      <p className="text-center">{"1"}</p>
+      <img
+        className="text-center w-40"
+        src={bodyPartImage}
+        alt={bodyPartName}
+      />
       <p className="font-Mona-Sans text-2xl text-content text-center">
         {bodyPartName}
       </p>
--- src/components/Workouts/ExerciseCarousel.jsx
+++ src/components/Workouts/ExerciseCarousel.jsx
@@ -1,11 +1,47 @@
 import { useEffect, useState } from "react";
 import { bodyPartsURL, exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseBodyPartCard from "./ExerciseBodyPartCard";
-import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline"; // Import icons
+import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { useTheme } from "../../contexts/ThemeContext";
+import {
+  chestImage,
+  chestImageDark,
+  backImage,
+  backImageDark,
+  upperArmsImage,
+  upperArmsImageDark,
+  shouldersImage,
+  shouldersImageDark,
+  lowerArmsImage,
+  lowerArmsImageDark,
+  lowerLegsImage,
+  lowerLegsImageDark,
+  upperLegsImage,
+  upperLegsImageDark,
+  waistImage,
+  waistImageDark,
+  neckImage,
+  neckImageDark,
+} from "../utils/constants";
 
 const ExerciseCarousel = () => {
+  const { theme } = useTheme();
   const [bodyParts, setBodyParts] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
+
+  const bodyPartImages = {
+    all: "all",
+    back: theme === "light" ? backImage : backImageDark,
+    cardio: "cardioImage",
+    chest: theme === "light" ? chestImage : chestImageDark,
+    "lower arms": theme === "light" ? lowerArmsImage : lowerArmsImageDark,
+    "lower legs": theme === "light" ? lowerLegsImage : lowerLegsImageDark,
+    neck: theme === "light" ? neckImage : neckImageDark,
+    shoulders: theme === "light" ? shouldersImage : shouldersImageDark,
+    "upper arms": theme === "light" ? upperArmsImage : upperArmsImageDark,
+    "upper legs": theme === "light" ? upperLegsImage : upperLegsImageDark,
+    waist: theme === "light" ? waistImage : waistImageDark,
+  };
 
   useEffect(() => {
     const fetchExerciseData = async () => {
@@ -30,11 +66,12 @@ const ExerciseCarousel = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex justify-center pt-20 pb-10 gap-60">
+      <div className="flex justify-center pt-20 pb-10 gap-x-60">
         {bodyParts.slice(startIndex, startIndex + 3).map((bodyPart, index) => (
           <ExerciseBodyPartCard
             key={startIndex + index}
             bodyPartName={bodyPart}
+            bodyPartImage={bodyPartImages[bodyPart]}
           />
         ))}
       </div>
