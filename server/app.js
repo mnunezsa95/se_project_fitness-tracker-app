f5038a9aee6fbbd34e1e1fdbc89aad90acd8dfb5
mnunezsa <mnunezsa95@gmail.com>
2024-09-04 18:07:49 -0400

    change backend server to module type instead of commonJS

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => console.log(`App is listening at port ${PORT}`));
