d994987a828143b0d2f000176e348e9310a4d0b5
mnunezsa <mnunezsa95@gmail.com>
2024-09-05 11:55:49 -0400

    add tool card compopnent

--- client/src/components/Tools/ToolsAndCalculators.jsx
+++ client/src/components/Tools/ToolsAndCalculators.jsx
@@ -1,24 +1,31 @@
 import ToolCard from "./ToolCard";
-import bmiCalculatorIcon from "../../images/bmi-calculator.svg";
-
-const tools = [
-  {
-    toolName: "BMI Calculator",
-    toolImg: bmiCalculatorIcon,
-  },
-  {
-    toolName: "Caloric Deficit Calculator",
-    toolImg: null,
-  },
-  {
-    toolName: "Daily Water Intake Calculator",
-    toolImg: null,
-  },
-];
+import bmiCalculatorDarkIcon from "../../images/bmi-calculator-dark.svg";
+import bmiCalculatorLightIcon from "../../images/bmi-calculator-light.svg";
+import { useTheme } from "../../contexts/ThemeContext";
 
 const ToolsAndCalculators = () => {
+  const { theme } = useTheme();
+
+  const tools = [
+    {
+      toolName: "BMI Calculator",
+      toolImg:
+        theme === "dark" ? bmiCalculatorDarkIcon : bmiCalculatorLightIcon,
+      toolDescription: "abc",
+    },
+    {
+      toolName: "Caloric Deficit Calculator",
+      toolImg: null,
+      toolDescription: "abc",
+    },
+    {
+      toolName: "Daily Water Intake Calculator",
+      toolImg: null,
+      toolDescription: "abc",
+    },
+  ];
   return (
-    <div className="flex justify-center text-content">
+    <div className="flex items-center justify-center h-[90vh] dark:text-content text-backgroundAccent">
       <div className="flex gap-20">
         {tools.map((tool, i) => {
           return <ToolCard tool={tool} key={i} />;
