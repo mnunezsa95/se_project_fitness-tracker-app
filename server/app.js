4510eb3984a45c73e4b10a43b9e0d5d9793c5380
mnunezsa <mnunezsa95@gmail.com>
2024-09-24 16:48:22 -0400

    install and connect postgresql database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,29 @@
-import express from "express";
+const express = require("express");
+const pool = require("./db");
 
+const PORT = 3001;
 const app = express();
-const PORT = 3002;
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const getUsers = (req, res) => {
+  pool.query("SELECT * FROM users", (err, results) => {
+    if (err) {
+      console.error("Error fetching students:", err);
+      return res.status(500).json({ error: "Database query error" });
+    }
+    return res.status(200).json(results.rows);
+  });
+};
+
+app.get("/students", getUsers);
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on Port ${PORT}`);
+  try {
+    await pool.query("SELECT 1");
+    console.log(`Connected to the sql database`);
+  } catch (err) {
+    console.error("SQL database connection failed:", err);
+    process.exit(1);
+  }
+});
